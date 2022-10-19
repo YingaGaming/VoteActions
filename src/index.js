356d8a50fdb14df316c2bf36e8cd5a2e055c38ba
Xenorio <hi@xenorio.xyz>
2022-10-19 08:01:02 +0200

    switch votifier package

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-const Votifier = require('votifier-server')
 const fs = require('fs')
 
 const config = require('../config')
 
-if (!fs.existsSync('./private.key')) {
-    console.log('RSA private key not found! Please make sure to put it as "private.key" into the root directory, or generate a new one using "npm run keygen"')
+if (!fs.existsSync('./private.pem')) {
+    console.log('RSA private key not found! Please make sure to put it as "private.pem" into the root directory, or generate a new one using "npm run keygen"')
+    process.exit()
 }
 
-let privateKey = fs.readFileSync('./private.key', 'utf-8')
-
-let votifier = new Votifier(privateKey, config.port)
+const votifier = require("votifier")(__dirname + "/private.pem", config.port);
 
 votifier.on('vote', function(username, server, ip, date) {
     console.log(username);
--- src/keygen.js
+++ src/keygen.js
@@ -1,15 +1,43 @@
 const NodeRSA = require('node-rsa')
+const { generateKeyPair, X509Certificate } = require('crypto')
 const fs = require('fs')
 
 console.log('Generating Keypair...')
 
-const key = new NodeRSA({
+generateKeyPair('rsa', {
+    modulusLength: 1024,
+    publicKeyEncoding: {
+        type: 'pkcs1',
+        format: 'pem'
+    },
+    privateKeyEncoding: {
+        type: 'pkcs1',
+        format: 'pem'
+    }
+}, (err, publicKey, privateKey) => {
+    if (err) {
+        console.error(err)
+        process.exit()
+    }
+    fs.writeFileSync('./private.key', privateKey)
+    fs.writeFileSync('./public.key', publicKey)
+});
+
+return
+
+let key = new NodeRSA({
     b: 2048,
     encryptionScheme: 'pkcs1'
 });
 
-let private = key.exportKey('pkcs1-private')
+//key = key.generateKeyPair()
+
+let private = key.exportKey('pkcs1')
+
 let public = key.exportKey('pkcs1-public')
+    .split('-----BEGIN RSA PUBLIC KEY-----').join('')
+    .split('-----END RSA PUBLIC KEY-----').join('')
+    .split('\n').join('')
 
 fs.writeFileSync('./private.key', private)
 fs.writeFileSync('./public.key', public)
